677e1cf8231b8442d807eda7460e7609113a2514
Jalen Arrington <jmarrington178@yahoo.com>
2023-01-14 22:08:18 -0500

    Corrected typos

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import math
 
 pygame.init()
 fps = 60
-timer = pygame.time.clock()
-font = pygame.font.font('assets/font/myFont.ttf', 32)
+timer = pygame.time.Clock()
+font = pygame.font.Font('assets/font/myFont.ttf', 32)
 WIDTH = 900
 HEIGHT = 800
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
@@ -13,9 +13,9 @@ banners = []
 guns = []
 level = 0
 for i in range(1,4):
-    bgs.append(pygame.image.loaf(f'assets/bgs/{i}.png'))
-    banners.append(pygame.image.loaf(f'assets/bgs/{i}.png'))
-    guns.append(pygame.image.loaf(f'assets/bgs/{i}.png'))
+    bgs.append(pygame.image.load(f'assets/bgs/{i}.png'))
+    banners.append(pygame.image.load(f'assets/bgs/{i}.png'))
+    guns.append(pygame.image.load(f'assets/bgs/{i}.png'))
 
 run = True
 while run:
