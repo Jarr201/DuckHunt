ad1dec1715976315c1a49dd27e798a7650bc1944
Jalen Arrington <jmarrington178@yahoo.com>
2023-01-14 21:59:37 -0500

    Set up for loops for bg, banner, and gun immage groups

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ bgs = []
 banners = []
 guns = []
 level = 0
+for i in range(1,4):
+    bgs.append(pygame.image.loaf(f'assets/bgs/{i}.png'))
+    banners.append(pygame.image.loaf(f'assets/bgs/{i}.png'))
+    guns.append(pygame.image.loaf(f'assets/bgs/{i}.png'))
